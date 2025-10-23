2c83a851551ee2086e6c726f58a393638ecdac63
4urora3night <auroraph4ntom@gmail.com>
2025-10-23 19:35:12 +0100

    feat: neovim config

---
+++ .config/nvim/propio-nvim-custom.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_transparent_background = 2
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+      opts.keymap = {
+        preset = "super-tab",
+        ["<S-Tab>"] = { "select_and_accept" },
+      }
+    end,
+  },
+}
